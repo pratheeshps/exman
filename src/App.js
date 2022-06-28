d328434e5fa28aabb85cd106b793425640c76d08
pratheesh.p <pratheesh.p@kycres.com>
2022-06-28 21:15:30 +0530

    table component style and adding icons

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import "./App.scss";
 
-import Table from "./commons/table";
+import Categories from "./categories";
 
 function App() {
 
-  const convertToDateString = (timestamp) => {
-    const date = new Date(timestamp);
-    return date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear()
-  }
+ 
 
   return (
     <div className="em-main-container">
-      <Table
-        headers={{ date: "Date", description: "Description", amount: "Amount" }}
-        rows={[
-          {
-            date: 1656350912000,
-            description: "Painting Putty",
-            amount: 2080,
-          },
-          {
-            date: 1655400512000,
-            description: "Electrical wires",
-            amount: 3450,
-          },
-          {
-            date: 1654104512000,
-            description: "Plumbing fittings",
-            amount: 1250,
-          }
-        ]}
-        footer={{ date: "", description: "Total", amount: 5530 }}
-        sorters={{
-          date: true,
-          description: true,
-          amount: true,
-        }}
-        processData={{
-          date: convertToDateString
-        }}
-      />
+      <Categories />
     </div>
   );
 }
